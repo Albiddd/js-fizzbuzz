6a06a545dbfeac36fc21b29c066d50ecae7a0358
Albiddd <alberto.dore93@gmail.com>
2022-07-22 15:30:53 +0200

    MILESTONE 3

--- js/app.js
+++ js/app.js
@@ -18,6 +18,35 @@
 
 // MILESTONE 2 
 // INSERISCO CON APPEND I RISULTATI DEL FOR SU UN ELEMENTO HTML
+// const gridElement = document.querySelector('.grid');
+// gridElement.innerHTML = '';
+
+
+// for(let i=1; i<=100; i++){
+//     let num = i;
+//     const divElement = document.createElement('div');
+    
+
+//     gridElement.append(divElement);
+
+//     if(num % 15 == 0){
+//         num = 'FizzBuzz';
+//     }
+//     else if(num % 3 == 0){
+//         num = 'Fizz';
+//     }
+//     else if(num % 5 == 0){
+//         num = 'Buzz';
+//     }
+    
+//     divElement.append(num)
+//     divElement.classList.add('cella');
+//     gridElement.append(divElement);
+// }
+
+
+// MILESTONE 3
+// APPLICO GLI STILI AGLI ELEMENTI 
 const gridElement = document.querySelector('.grid');
 gridElement.innerHTML = '';
 
@@ -25,26 +54,25 @@ gridElement.innerHTML = '';
 for(let i=1; i<=100; i++){
     let num = i;
     const divElement = document.createElement('div');
-    
+    const contentElement = document.createElement('div');
 
     gridElement.append(divElement);
+    divElement.append(contentElement);
 
     if(num % 15 == 0){
         num = 'FizzBuzz';
+        divElement.classList.add('fizz-buzz');
     }
     else if(num % 3 == 0){
         num = 'Fizz';
+        divElement.classList.add('fizz');
     }
     else if(num % 5 == 0){
         num = 'Buzz';
+        divElement.classList.add('buzz');
     }
     
-    divElement.append(num)
+    contentElement.append(num);
     divElement.classList.add('cella');
     gridElement.append(divElement);
 }
-
-
-// MILESTONE 3
-// APPLICO GLI STILI AGLI ELEMENTI 
-
