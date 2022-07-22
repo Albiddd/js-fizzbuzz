11880b771915f7bc1689acba45e7903dc9ee351f
Albiddd <alberto.dore93@gmail.com>
2022-07-22 15:06:25 +0200

    milestone 2

--- js/app.js
+++ js/app.js
@@ -14,3 +14,30 @@ for(let i=1; i<=100; i++){
     }
     console.log(num);
 }
+
+const gridElement = document.querySelector('.grid');
+gridElement.innerHTML = '';
+
+
+for(let i=1; i<=100; i++){
+    let num = i;
+    const divElement = document.createElement('div');
+    
+
+    gridElement.append(divElement);
+
+    if(num % 15 == 0){
+        num = 'FizzBuzz';
+    }
+    else if(num % 3 == 0){
+        num = 'Fizz';
+    }
+    else if(num % 5 == 0){
+        num = 'Buzz';
+    }
+    
+    divElement.append(num)
+    divElement.classList.add('cella');
+    gridElement.append(divElement);
+}
+
